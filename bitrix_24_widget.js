6598b86b2e85f86d1f643433178b98e305b4215d
Александр Кузьмин <60044563+aleksandrkuzminv@users.noreply.github.com>
2020-08-28 12:36:27 +0300

    Delete bitrix_24_widget.js

--- bitrix_24_widget.js
+++
@@ -1,4 +0,0 @@
-(function(w,d,u){
-                var s=d.createElement(&#39;script&#39;);s.async=true;s.src=u+&#39;?&#39;+(Date.now()/60000|0);
-                var h=d.getElementsByTagName(&#39;script&#39;)[0];h.parentNode.insertBefore(s,h);
-        })(window,document,&#39;https://cdn-ru.bitrix24.ru/b12633332/crm/site_button/loader_2_o4mcup.js&#39;);
